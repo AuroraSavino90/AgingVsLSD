0a10f64e1eddf71e4ceba38e58acac2653b61a72
Aurora <aurorasavino42@gmail.com>
2026-02-24 09:17:39 +0100

    save version of loaded packages

--- Scripts/Packages.R
+++ Scripts/Packages.R
@@ -13,3 +13,12 @@ library(org.Hs.eg.db)
 library(slinky)
 library(GSVA)
 library(GEOquery)
+
+# Save versions of loaded packages
+pkgs <- sessionInfo()$otherPkgs
+pkg_versions <- data.frame(
+  Package = names(pkgs),
+  Version = sapply(pkgs, function(x) x$Version)
+)
+
+write.csv(pkg_versions, "package_versions.csv", row.names = FALSE)
